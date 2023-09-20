c9079a5f4147566620e7b140487cfde356dad7e2
Djordje <djordje.thai@gmail.com>
2023-09-20 14:16:00 +0700

    tab

--- uputstvo.py
+++ uputstvo.py
@@ -5,13 +5,24 @@ chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=["a", "b", "c"])
 st.title("Uputstvo za koriscenje aplikacije")
-st.subheader("Ovde ide podnaslov")
+st.subheader("Ovo moze da bude vrsta menija (ili kao buttons)")
+tab1, tab2, tab3 = st.tabs(["Vizija", "Misija", "Ciljevi"])
+
+with tab1:
+   st.header("Vizija")
+   st.info("Positive je kompanija posvećena poštovanju svojih vrednosti, misije, vizije i ciljeva, koji su usmereni na osiguravanje budućeg uspeha kompanije. ")
+
+with tab2:
+   st.header("Misija")
+   st.success("Sa tačke gledišta zaposlenih u Positive-u, cilj je postizanje ravnoteže između posla i privatnog života zaposlenih, smanjenje nivoa stresa na minimum i obezbeđivanje zarade koja omogućava da oni i njihove porodice uživaju u kvalitetnom i zdravom životu. Kompanija ima za cilj da svojim zaposlenima pruži ne samo stručna znanja, već i životne veštine koje će im pomoći da postanu bolje osobe, stvarajući tako srećno radno okruženje u kojem su svi zaposleni zadovoljni. To osigurava da će se fokusirati na potrebe klijenata i sa zadovoljstvom obavljati svoj posao.")
+
+with tab3:
+    st.header("Ciljevi")
+    st.info("Pre svega, povećanje poslovnih performansi. Produktivnosti i efikasnosti. Smanjenje troškova i unapređenje znanja. Bezbedno i legalno poslovanje primenom  standarda kvaliteta. Praćenjem trenda digitalne transformacije. Za kraj, podizanje informatičke svesti i edukacija tržišta. A iza svega, sertifikovani stučnjaci, lideri iz oblasti informacionih sistema.")
 col1, col2 = st.columns(2)
 with col1:
     with st.expander("Uputstvo za koriscenje aplikacije (otvorite da vidite detalje)", expanded=False):
         st.write("Ovde ide uputstvo za koriscenje aplikacije")
-        st.info("Ovde ide info poruka")
-        st.success("Ovde ide potvrda uspeha")
         st.warning("Ovde ide upozorenje")
         st.error("Ovde erro message")
         st.image("https://test.georgemposi.com/wp-content/uploads/2023/05/positive-logo-red.jpg%22", width=200)
